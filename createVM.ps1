ef25bc8864277f1969bd5e57408b61cd8130ef47
Guilherme Ferreira <barret.gui@hotmail.com>
2021-01-11 17:02:29 -0300

    Update createVM.ps1

---
+++ createVM.ps1
@@ -0,0 +1,14 @@
+Param(
+    [Parameter(Position=1)]
+    [string]$Server,
+
+    [Parameter(Position=2)]
+    [string]$Username,
+
+    [Parameter(Position=3)]
+    [string]$Password
+)
+
+Write-Host $Server
+Write-Host $Username
+Write-Host $Password
