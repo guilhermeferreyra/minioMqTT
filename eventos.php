b7c40a377be019a81f91f9faf55424c29219bb2e
Guilherme Ferreira <barret.gui@hotmail.com>
2021-01-18 13:40:58 -0300

    Create eventos.php

---
+++ eventos.php
@@ -0,0 +1,23 @@
+<?php
+
+$client = new Mosquitto\Client('Minio');
+
+$client->setCredentials('infiniit','1nfini!T');
+ 
+
+$client->onConnect(function($code, $message) use ($client){
+    
+    $client->subscribe('minio/eventos', 0);
+});
+
+$client->onMessage(function($message){
+    
+    $jsonPayload = json_decode($message->payload);
+    echo $jsonPayload
+    }
+});
+
+$client->connect('localhost', 1885);
+
+$client->loopForever();
+?>
